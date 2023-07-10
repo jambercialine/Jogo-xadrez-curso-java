66f1609807ad8b5b28716c22a97fc59899ea1377
Aline Jamberci <jambercialine@outlook.com>
2023-07-10 13:28:47 -0300

    Jogada especial: Promoção.

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,11 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				if(chessMatch.getPromoted() != null) {
+					System.out.println("Entre com a peça para promoção: (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
